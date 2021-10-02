2d624fbe6cf0776ceca066013f6ae62d9e348875
Aflah <aflah20082@iiitd.ac.in>
2021-10-03 02:02:22 +0530

    Done till 2nd Oct

--- Function and Arrays/SubarrayProblem.java
+++ Function and Arrays/SubarrayProblem.java
@@ -1,23 +1,24 @@
-//import java.util.Scanner;
-//
-//public class SubarrayProblem {
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        int n = sc.nextInt();
-//        int[] arr = new int[n];
-//        for (int i = 0; i < n; i++){
-//            arr[i] = sc.nextInt();
-//        }
-//        for (int i = 0; i < n; i++){
-//            int x = i+1;
-//            while (x < n){
-//                for (int j = i; j <= x; j++){
-//                    System.out.print(arr[j] + " ");
-//                }
-//                x++;
-//                System.out.println();
-//            }
-//            System.out.println();
-//        }
-//    }
-//}
+import java.util.Scanner;
+
+public class SubarrayProblem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int arrSize = arr.length;
+        for (int startPoint = 0; startPoint < arrSize; startPoint++)
+        {
+            for (int grps = startPoint + 1; grps <= arrSize; grps++)
+            {
+                for (int j = startPoint; j < grps; j++)
+                {
+                    System.out.print(arr[j] + "	");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
